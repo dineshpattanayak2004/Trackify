e0c06428116fd642ea34f881248c7956acc16d76
Dinesh Pattanayak <dineshpattanayak0407@gmail.com>
2026-07-08 22:26:48 +0530

    fix: add ErrorBoundary to main.jsx for catching runtime errors

--- src/main.jsx
+++ src/main.jsx
@@ -2,12 +2,15 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 
 import App from "./App";
+import ErrorBoundary from "./components/ErrorBoundary";
 import "./index.css";
 
 ReactDOM.createRoot(
- document.getElementById("root")
+  document.getElementById("root")
 ).render(
- <React.StrictMode>
-   <App />
- </React.StrictMode>
+  <React.StrictMode>
+    <ErrorBoundary>
+      <App />
+    </ErrorBoundary>
+  </React.StrictMode>
 );
